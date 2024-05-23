5d93ddb153b31d7215d4d87309cb7fb4f9dd544f
hizv <18361766+hizv@users.noreply.github.com>
2024-05-23 18:03:25 +0530

    Save dark mode state

--- js.js
+++ js.js
@@ -1,4 +1,23 @@
+const getInitialState = () => {
+  const initialValue = localStorage.getItem("darkMode");
+
+  if(initialValue === null){
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  return initialValue == "true";
+}
+
+let darkMode = getInitialState();
+
+if (darkMode) {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
 function toggle_dark_mode() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
+  darkMode = !darkMode; //invert our variable
+  localStorage.setItem("darkMode", darkMode); //save it in local storage
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
